c19d03e49cb9909370f692ecad47e51ba25db7b2
feliperezn <rezfelipe@outlook.com>
2026-08-18 20:27:20 -0300

    Fix: Accelerate method

--- Aulas/2026-08-18-aula/app/src/main/java/ads/poo/Car.java
+++ Aulas/2026-08-18-aula/app/src/main/java/ads/poo/Car.java
@@ -4,25 +4,24 @@ public class Car {
   // properties
   private int currentSpeed;
   private int maxSpeed;
-  
-  // methods
-  public void accelerate(int increment){
 
-    currentSpeed += increment;
-    
-    if (currentSpeed > maxSpeed) {
+  // methods
+  public void accelerate(int increment) {
+
+    if ((currentSpeed + increment) > maxSpeed) {
       currentSpeed = maxSpeed;
+    } else {
+      currentSpeed += increment;
     }
-    
+
   }
 
-  public void setMaxSpeed(int speed){
+  public void setMaxSpeed(int speed) {
     maxSpeed = speed;
   }
 
-  public int getCurrentSpeed(){
+  public int getCurrentSpeed() {
     return currentSpeed;
   }
-
 
 }
